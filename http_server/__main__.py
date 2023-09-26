898a50c806999b6c18c2e04bae93587c411cffda
Ryan Schanzenbacher <ryan@rschanz.org>
2023-09-25 23:38:21 -0400

    just need to implement the response constructor

--- http_server/__main__.py
+++ http_server/__main__.py
@@ -18,23 +18,23 @@ def init():
     # Now that we have a request, let's parse it and send it to a handler
     if req.method == "GET":
         # Send to GET handler
-        get.process_req(req)
+        return get.process_req(req)
     elif req.method == "POST":
         # Send to POST handler
-        post.process_req(req)
+        return post.process_req(req)
     elif req.method == "PUT":
         # Send to PUT handler
-        put.process_req(req)
+        return put.process_req(req)
     elif req.method == "DELETE":
         # Send to DELETE handler
-        delete.process_req(req)
+        return delete.process_req(req)
     elif req.method == "HEAD":
         # Send to HEAD handler
-        head.process_req(req)
+        return head.process_req(req)
     else:
         # Unimplemented method, return a 400 BAD REQUEST response
-        print(HTTPResponse(HTTPStatusCode.BAD_REQUEST))
+        return HTTPResponse(HTTPStatusCode.BAD_REQUEST)
 
 
 if __name__ == "__main__":
-    init()
+    print(init()) # print used here as placeholder for socket connection
--- http_server/handlers/get.py
+++ http_server/handlers/get.py
@@ -1,4 +1,13 @@
 # Handler for GET requests
+import os
+from http_server.response.response import HTTPResponse
+from http_server.response.codes import HTTPStatusCode
 
 def process_req(request):
+    # Get the file that is being requested. Web server ROOT is determined by
+    # launch parameters. See __main__.py for details
     print(request)
+    
+    # TODO
+    # will always return 200 on a valid request
+    return HTTPResponse(HTTPStatusCode.OK,content="This is the body of a response!")
